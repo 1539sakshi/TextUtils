82a21e8e1155df8755d2b844d4b07fe4ec36ca5a
tush-rastogi <60700955+tush-rastogi@users.noreply.github.com>
2020-10-01 00:10:49 +0530

    improve code

--- textutils/asgi.py
+++ textutils/asgi.py
@@ -7,6 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
+
+
+
 import os
 
 from django.core.asgi import get_asgi_application
@@ -14,3 +17,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'textutils.settings')
 
 application = get_asgi_application()
+# nice work ..go ahead
